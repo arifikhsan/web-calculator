50d05bbe5a9954b7f6900fff029391f7af323830
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2021-02-21 23:13:16 +0700

    calc

--- assets/kalkulator.js
+++ assets/kalkulator.js
@@ -97,5 +97,13 @@ function performCalculation() {
       parseInt(calculator.firstNumber) - parseInt(calculator.displayNumber);
   }
 
+  const history = {
+    firstNumber: calculator.firstNumber,
+    secondNumber: calculator.displayNumber,
+    operator: calculator.operator,
+    result: result,
+  };
+  putHistory(history);
   calculator.displayNumber = result;
+  renderHistory();
 }
